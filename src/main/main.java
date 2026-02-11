5579609db01c5ce44b99a863a640c9811b2b12ff
Alejandro Acosta Arencibia <aacostaarencibia@gmail.com>
2026-02-11 16:24:59 +0000

    Correcion main, para que funcione y adaptacion de los articulos generados para que sean mas adecuados al programa

--- src/main/main.java
+++ src/main/main.java
@@ -1,10 +1,10 @@
 /** @author AlejandroAcosta */
-public class Main {
+public class main {
     public static void main(String[] args) {
         Pedido miPedido = new Pedido("Pepe Suarez");
 
-        Articulo art1 = new Articulo("Teclado RGB", 1, 49.99);
-        Articulo art2 = new Articulo("Ratón Inalámbrico", 2, 19.50);
+        Articulo art1 = new Articulo("Pizza 4 quesos", 1, 19.99);
+        Articulo art2 = new Articulo("Agua con Gas", 2, 1.50);
 
         miPedido.agregarArticulo(art1);
         miPedido.agregarArticulo(art2);
