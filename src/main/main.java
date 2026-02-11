63f0ff6e0d8830a9882806dde1c4171595ab0957
Alejandro Acosta Arencibia <aacostaarencibia@gmail.com>
2026-02-11 16:40:04 +0000

    Cambio nombre de cliente + cambio de simbolo de euro a dolar ya que el euro da en consola un ?

--- src/main/main.java
+++ src/main/main.java
@@ -1,7 +1,7 @@
 //** @author AlejandroAcosta */
 public class main {
     public static void main(String[] args) {
-        Pedido miPedido = new Pedido("Pepe Suarez");
+        Pedido miPedido = new Pedido("Alberto Chicote");
 
         Articulo art1 = new Articulo("Pizza 4 quesos", 1, 19.99);
         Articulo art2 = new Articulo("Agua con Gas", 2, 1.50);
@@ -11,7 +11,7 @@ public class main {
 
         System.out.println("=== SISTEMA DE PEDIDOS ===");
         System.out.println("Cliente: " + miPedido.getCliente());
-        System.out.println("Total Pedido: " + miPedido.calcularTotal() + "€");
+        System.out.println("Total Pedido: " + miPedido.calcularTotal() + "$");
         System.out.println("Estado actual: " + miPedido.getEstado());
 
         System.out.println("\nProcesando pedido...");
